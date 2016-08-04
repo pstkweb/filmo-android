4be72ef6f7d1e32f4cee989903e79740ed9cdd1b
pstkweb <github61>
2016-08-05 00:58:22 +0200

    App Toolbar expanded with FAB

--- app/src/main/java/com/sixfingers/filmo/MainActivity.java
+++ app/src/main/java/com/sixfingers/filmo/MainActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
 
 import com.sixfingers.filmo.adapter.MoviesCardAdapter;
 import com.sixfingers.filmo.dvdfrapi.DVDFrService;
@@ -12,6 +15,7 @@ import com.sixfingers.filmo.dvdfrapi.models.DVDResult;
 import com.sixfingers.filmo.dvdfrapi.models.SearchResult;
 import com.sixfingers.filmo.dvdfrapi.models.SupportType;
 import com.sixfingers.filmo.model.Movie;
+import com.sixfingers.ui.floatingactionmenu.FloatingActionMenu;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -21,6 +25,7 @@ import retrofit2.converter.simplexml.SimpleXmlConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
     private MoviesCardAdapter cardsListAdapter;
+    private FloatingActionMenu appFAM;
 
     private class SearchTask extends AsyncTask<String, Void, SearchResult> {
         @Override
@@ -48,10 +53,21 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public void afficherCards(SearchResult result) {
+        for (DVDResult dvd : result.getDVDs()) {
+            cardsListAdapter.addItem(dvd.toMovie(), cardsListAdapter.getItemCount());
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        appFAM = (FloatingActionMenu) findViewById(R.id.FAM);
 
         RecyclerView cardsList = (RecyclerView) findViewById(R.id.cards_recycler);
         cardsList.setHasFixedSize(true);
@@ -63,12 +79,27 @@ public class MainActivity extends AppCompatActivity {
         cardsListAdapter.setHasStableIds(true);
         cardsList.setAdapter(cardsListAdapter);
 
+        cardsList.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                if (dy > 0) {
+                    // show FAM
+
+                } else {
+                    // hide FAM
+                }
+            }
+        });
+
         //new SearchTask().execute("Batman");
     }
 
-    public void afficherCards(SearchResult result) {
-        for (DVDResult dvd : result.getDVDs()) {
-            cardsListAdapter.addItem(dvd.toMovie(), cardsListAdapter.getItemCount());
-        }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.home_menu, menu);
+
+        return true;
     }
 }
